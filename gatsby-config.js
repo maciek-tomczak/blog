e97c02bacfa523f13513ca0adf72c9c88e7a6fcb
Razjel <maciektomr@gmail.com>
2020-06-15 22:29:28 +0900

    added gatsby serve --prefix-paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const urljoin = require("url-join")
 const siteConfig = require("./siteConfig")
 
 module.exports = {
+	pathPrefix: `/maciek.github.io`,
   siteMetadata: {
     title: siteConfig.name,
     author: siteConfig.author,
