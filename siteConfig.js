3e6e10429690fa5f8aa01647cbb6e3e13428f12a
Razjel <maciektomr@gmail.com>
2020-06-15 21:53:01 +0900

    change config

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Maciek Tomczak',
   shortName: 'Maciek',
   description: 'Hi! I am Maciek and this site is accompanying work I like to do.',
-  url: 'https://www.jooplaan.com', // Domain of your site without prefix!
+  url: 'https://maciek-tomczak.github.io/maciek.github.io', // Domain of your site without prefix!
   prefix: '/',
   author: 'Maciek'
 }
