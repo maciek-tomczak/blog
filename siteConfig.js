91609bb05492763cbd40097723228baafc5c9d46
Razjel <maciektomr@gmail.com>
2020-06-17 03:51:48 +0900

    add prefix to build in package.json

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Maciek Tomczak',
   shortName: 'Maciek',
   description: 'I invite you to check out what I\'ve been up to.',
-  url: 'https://maciek-tomczak.github.io', // Domain of your site without prefix!
+  url: 'https://maciek-tomczak.github.i', // Domain of your site without prefix!
   prefix: '/',
   author: 'Maciek'
 }
